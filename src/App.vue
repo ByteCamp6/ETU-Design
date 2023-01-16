fdd7c4016a57442d8d16380e4e816451d04add94
voilt-sourse <2503063984@qq.com>
2023-01-16 15:13:36 +0800

    🌈 style: 更新了代码格式

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
 
 <script setup lang="ts">
 import Button from "./components/Button.vue";
-import Divider from "./components/Divider.vue"
-import Breadcrumb from "./components/breadcrumb.vue";
+import Divider from "./components/Divider.vue";
+import Breadcrumb from "./components/Breadcrumb.vue";
 </script>
 
 <style scoped></style>
