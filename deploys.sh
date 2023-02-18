e56f111ab60f695e9cf80fc8e47e07879acd3a9a
王帆 <9729143+wang-fan1111@user.noreply.gitee.com>
2023-02-18 21:40:57 +0800

    deploy

--- deploys.sh
+++ deploys.sh
@@ -5,5 +5,5 @@ cd docs/.vitepress/dist  # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAMEgit init
 git add -A
 git commit -m 'deploy' # 如果部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master# 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pagescd -
