7d7ad8eb4cd78e1a24dbfddc03a753ba6863cf92
liuyuyang <2281487673@qq.com>
2023-02-18 16:22:55 +0800

    add(docs)增加了Drawer的说明文档及演示

---
+++ docs/examples/drawer/titleSolt.vue
@@ -0,0 +1,18 @@
+<template>
+  <etu-drawer direction="Left" v-model="valLeft" Tittle="自定义的标题">
+    Etu-Design的Etu是esay to use的缩写
+  </etu-drawer>
+  <etu-button @click="LeftShow">抽屉</etu-button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+let valLeft = ref(false);
+function LeftShow() {
+  valLeft.value = !valLeft.value;
+  console.log("左抽屉");
+}
+</script>
+
+<style scoped></style>
