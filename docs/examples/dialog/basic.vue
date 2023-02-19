e0aac724f4cd933d096ee4a240e50ced6c4b6204
1379255913 <1379255913@qq.com>
2023-02-19 13:27:06 +0800

    add(docs/dialog) 完成dialog的文档

---
+++ docs/examples/dialog/basic.vue
@@ -0,0 +1,21 @@
+<template>
+  <etu-button text @click="dialogVisible = true"> 点击打开对话框 </etu-button>
+
+  <etu-dialog v-model="dialogVisible" title="Tips" width="30%">
+    <span>这里是消息</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <etu-button @click="dialogVisible = false">取消</etu-button>
+        <etu-button type="primary" @click="dialogVisible = false">
+          确认
+        </etu-button>
+      </span>
+    </template>
+  </etu-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const dialogVisible = ref(false);
+</script>
---
+++ docs/examples/dialog/draggable.vue
@@ -0,0 +1,22 @@
+<template>
+  <etu-button @click="dialogVisible = true">打开dialog</etu-button>
+  <etu-dialog v-model="dialogVisible" title="Tips" width="30%" draggable>
+    <span
+      >设置滚动锁定时，由于滚动条消失，页面的宽度会发生改变，我通过模拟计算修正了页面宽度，但是必须设置body的margin为0，否则会看到抖动的情况</span
+    >
+    <template #footer>
+      <span class="dialog-footer">
+        <etu-button @click="dialogVisible = false">Cancel</etu-button>
+        <etu-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </etu-button>
+      </span>
+    </template>
+  </etu-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const dialogVisible = ref(false);
+</script>
