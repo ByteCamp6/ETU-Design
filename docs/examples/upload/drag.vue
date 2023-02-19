ce4038595f5f8b8b6c47f7ce69789655c51681dc
1379255913 <1379255913@qq.com>
2023-02-19 21:15:17 +0800

    add(docs/upload) 添加upload的文档

---
+++ docs/examples/upload/drag.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <etu-upload
+      v-model:file-list="fileList"
+      class="upload-demo"
+      action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+      name="files"
+      :auto-upload="true"
+      drag
+      multiple
+      accept="image/png, image/jpeg"
+    >
+      <template #tip>
+        <div>格式要求：png/jpg</div>
+      </template>
+    </etu-upload>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const fileList = ref();
+</script>
