80b5c665d12540f1b5106b2db4d3d16ba1274c2e
jiaze <1244153559@qq.com>
2023-02-20 16:54:31 +0800

    add(input文档):新增input输入框组件的文档

---
+++ docs/examples/input/clearable.vue
@@ -0,0 +1,8 @@
+<template>
+  <etu-input v-model="input" placeholder="Please input" :clearable="true" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const input = ref("");
+</script>
