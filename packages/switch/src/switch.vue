83480b25add8868823336bb36c7811f55831a8fa
1379255913 <1379255913@qq.com>
2023-02-20 16:53:12 +0800

    fix(switch) 修复switch的bug并且完善文档

--- packages/switch/src/switch.vue
+++ packages/switch/src/switch.vue
@@ -18,7 +18,7 @@
 import { useDisabled, useNamespace } from "../../hooks";
 const bem = useNamespace("Switch");
 import { computed, ref } from "vue";
-import { isBoolean, isUndefined } from "@etu-design/utils";
+import { isBoolean, isPropAbsent, isUndefined } from "@etu-design/utils";
 
 // const emit = defineEmits(switchEmits);
 const emits = defineEmits<{
@@ -40,7 +40,7 @@ const props = defineProps({
   },
   modelValue: {
     type: [Boolean, String, Number],
-    default: false,
+    default: null,
   },
   disabled: {
     type: Boolean,
@@ -61,13 +61,13 @@ const props = defineProps({
 });
 const innerValue = ref<boolean | string | number>(false);
 const valueBridge = computed(() => {
-  return isUndefined(props.modelValue) ? props.modelValue : innerValue.value;
+  return !isPropAbsent(props.modelValue) ? props.modelValue : innerValue.value;
 });
 const isCheck = computed<boolean>(() => {
-  if (isBoolean(innerValue.value)) {
-    return innerValue.value;
+  if (isBoolean(valueBridge.value)) {
+    return valueBridge.value;
   } else {
-    return props.activeValue === innerValue.value;
+    return props.activeValue === valueBridge.value;
   }
 });
 const isDisabled = useDisabled();
@@ -79,15 +79,17 @@ const tClass = computed(() => {
   ];
 });
 const handleChange = (e: Event): void => {
+  console.log(3);
   const target = e.target as HTMLInputElement;
   const checked = target.checked;
   const value = checked
-    ? isUndefined(props.activeValue)
+    ? isPropAbsent(props.activeValue)
       ? checked
       : props.activeValue
-    : isUndefined(props.inactiveValue)
+    : isPropAbsent(props.inactiveValue)
     ? checked
     : props.inactiveValue;
+  console.log(value);
   emits("change", value);
   emits("update:modelValue", value);
   innerValue.value = value;
