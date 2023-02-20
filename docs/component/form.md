1b0f5229ca6e344a8d1b14ae212ad7ce67497496
jiaze <1244153559@qq.com>
2023-02-20 17:31:07 +0800

    add(form文档):新增form表单组件文档

---
+++ docs/component/form.md
@@ -0,0 +1,22 @@
+---
+title: Form
+---
+
+# Form 表单
+
+表单包含 输入框, 单选框, 下拉选择, 多选框 等用户输入的组件。 使用表单，您可以收集、验证和提交数据。
+
+## 典型表单
+
+最基础的表单包括各种输入表单项，比如 input、select、radio、checkbox 等。
+<preview path="../examples/form/basic.vue" title="" description=""></preview>
+
+## 表单校验
+Form 组件允许你验证用户的输入是否符合规范，来帮助你找到和纠正错误。
+
+Form 组件提供了表单验证的功能，只需为 rules 属性传入约定的验证规则，并将 form-Item 的 prop 属性设置为需要验证的特殊键值即可。 更多高级用法可参考 async-validator。
+<preview path="../examples/form/check.vue" title="" description=""></preview>
+
+## 自定义校验规则
+这个例子中展示了如何使用自定义验证规则来完成密码的二次验证。
+<preview path="../examples/form/validate.vue" title="" description=""></preview>
---
+++ docs/examples/form/check.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <etu-form ref="formRef" :model="formData" :rules="rules">
+      <etu-form-item label="名称1" prop="name" :rules="rules.name">
+        <etu-input v-model="formData.name"></etu-input>
+      </etu-form-item>
+      <etu-form-item label="名称2" prop="name1" :rules="rules.name1">
+        <etu-input v-model="formData.name1"></etu-input>
+      </etu-form-item>
+      <etu-form-item>
+        <etu-button @click.prevent="onSubmit" type="primary">提交</etu-button>
+      </etu-form-item>
+    </etu-form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+
+const formRef = ref();
+const formData = reactive({ name: "", name1: "" });
+const rules = {
+  name: [{ required: "true", message: "请输入名称", trigger: "blur" }],
+  name1: [{ required: "true", message: "请输入名称", trigger: "blur" }],
+};
+
+const onSubmit = () => {
+  const form = formRef.value;
+  form?.validate((validate, errors) => {
+    // console.log(validate, errors);
+  });
+};
+</script>
