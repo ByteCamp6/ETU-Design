3d5d30ea096a5839d41abd16773a84d60eee6ac5
jiaze <1244153559@qq.com>
2023-09-02 21:31:53 +0800

    chore: 删除无用代码

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,9 @@
-// import { createRouter, createWebHistory } from "vue-router";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "../packages/theme-chalk/src/index.scss";
-// import "../dist/theme-chalk/index.css";
-// const router = createRouter({
-//   history: createWebHistory(),
-//   routes: [
-//     {
-//       path: "/home",
-//       name: "Home",
-//       meta: {
-//         title: "首页",
-//         keepAlive: true,
-//         requireAuth: true,
-//       },
-//       component: () => import("@/components/Breadcrumb.vue"),
-//     },
-//   ],
-// });
 
 import Etu from "@etu-design/components";
-// import "./assets/main.css";
 
 const app = createApp(App);
 app.use(Etu);
-// app.use(router);
 app.mount("#app");
